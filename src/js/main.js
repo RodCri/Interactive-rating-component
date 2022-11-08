a466ed8fc274c1daa5694d888903e3816a45f6df
RodCri <cristian.rodriguez1317@gmail.com>
2022-11-08 08:05:42 -0500

    add function show modal and score selected

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
-//main.js file
+const submit = document.querySelector('.card__submit');
+const scores = document.querySelectorAll('.btn-score');
+const modal = document.querySelector('.modal');
+const card = document.querySelector('.card');
+const selectedScore = document.querySelector('.selected');
+
+let selected = 0;
+
+scores.forEach(element =>{
+  element.addEventListener('click', (event)=>{
+    selected = event.target.value;
+    console.log(selected)
+  });
+});
+
+submit.addEventListener('click', ()=>{
+  card.style.display = 'none';
+  modal.style.display = 'flex';
+  selectedScore.innerHTML = selected;
+});
